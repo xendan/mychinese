abc021fcc315066320678f29efe9c37cf53f83f4
Kseniia Mullomukhametova <kseniia_mullomukhametova@epam.com>
2016-07-04 21:45:46 +0300

    added lesson scripts

--- mychinese/settings.py
+++ mychinese/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mychinese',
         'USER':'root',
-        'PASSWORD':'myxaxa',
+        'PASSWORD':'secret',
         }
 }
 
